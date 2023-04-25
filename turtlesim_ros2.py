b3ccbb685e290968727dfdd09e095cb86bf99700
henriquemarlon <89201795+henriquemarlon@users.noreply.github.com>
2023-04-24 23:12:13 -0300

    Update turtlesim_ros2.py

--- turtlesim_ros2.py
+++ turtlesim_ros2.py
@@ -2,56 +2,58 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
+
 class TurtleController(Node):
     def __init__(self):
         super().__init__('turtle_controller')
         self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 1)
         self.timer_ = self.create_timer(0.1, self.diamond)
         self.twist_msg_ = Twist()
-        self.timer = 0
-        
+        self.counter = 0
+
     def first_movement(self):
         self.twist_msg_.linear.y = 4.0
         self.twist_msg_.linear.x = 2.0
         self.publisher_.publish(self.twist_msg_)
-        self.timer += 1
-        
+        self.counter += 1
+
     def second_movement(self):
         self.twist_msg_.linear.y = -4.5
         self.twist_msg_.linear.x = 1.0
         self.publisher_.publish(self.twist_msg_)
-        self.timer += 1
-        
+        self.counter += 1
+
     def third_movement(self):
         self.twist_msg_.linear.y = -4.0
         self.twist_msg_.linear.x = -2.0
         self.publisher_.publish(self.twist_msg_)
-        self.timer += 1
-        
+        self.counter += 1
+
     def fourth_movement(self):
         self.twist_msg_.linear.y = 2.0
         self.twist_msg_.linear.x = -0.5
         self.publisher_.publish(self.twist_msg_)
-        self.timer += 1
+        self.counter += 1
 
     def diamond(self):
-        if (self.timer < 10):
+        if (self.counter < 10):
             self.first_movement()
-            
-        if (self.timer >= 10 and self.timer < 20):
+
+        if (self.counter >= 10 and self.counter < 20):
             self.second_movement()
-            
-        if (self.timer >= 20 and self.timer < 30):
+
+        if (self.counter >= 20 and self.counter < 30):
             self.third_movement()
-        
-        if (self.timer >= 30 and self.timer < 40):
+
+        if (self.counter >= 30 and self.counter < 40):
             self.fourth_movement()
 
+
 def main():
-    print("The turtle is moving")
     rclpy.init()
     turtle_controller = TurtleController()
     rclpy.spin(turtle_controller)
+    print("The turtle is moving")
     turtle_controller.destroy_node()
     rclpy.shutdown()
 
@@ -62,4 +64,3 @@ if __name__ == '__main__':
     except Exception as e:
         print("The turtle stopped")
         print(e)
-
